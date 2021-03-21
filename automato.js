2da3e5140844bf8a21d6fd80355b28abdaa293d7
Bárbara Boechat <barbs.boechat@gmail.com>
2021-03-20 22:37:54 -0300

    automato e animações do grafo ok

--- automato.js
+++ automato.js
@@ -1,3 +1,9 @@
+/*
+* Autor: Bárbara Boechat
+* Data: 20/03/2021
+* */
+
+
 /* Construtor do Automato */
 class Automato {
     constructor(estados, transicoes, alfabeto) {
@@ -25,7 +31,6 @@ class Automato {
     le_transicao(estado, leitura) {
         let prox_estado = this.transicoes[estado][leitura]
         if (prox_estado === undefined) {
-            console.log('trans. indf para ', estado)
             return 'Indefinido'
         } else {
             return prox_estado
@@ -42,10 +47,13 @@ class Automato {
 }
 
 
-/* handler da UI */
+/* Controlador dos prints de leitura das
+* transições do automato */
+let output_count = 0
+
 class HandleUI {
     static out_transicao(comando, estado, leitura, prox_estado) {
-        let output = $('#saida-afd');
+        let output = $('#sub-saida' + output_count);
         let css_pill = 'afd-pill-' + comando
 
         output.append(
@@ -60,8 +68,12 @@ class HandleUI {
     static output_inicial(estado_inicial) {
         let output = $('#saida-afd');
         output.append(
-            `<div class="afd-item">
-                <span class="badge rounded-pill afd-pill-t afd-pill">${estado_inicial}</span>
+            `<div class="col-lg-3 afd-trans mt-2">
+                <div id="sub-saida${output_count}" class="output-area" style="margin-left: 0; margin-right: 0;">
+                    <div class="afd-item">
+                        <span class="badge rounded-pill afd-pill-t afd-pill">${estado_inicial}</span>
+                    </div>
+                </div>
             </div>`
         )
     }
@@ -70,7 +82,7 @@ class HandleUI {
         let icon = "";
         let output = $(element);
 
-        (comando) ? icon = 'icon-aceita.png': icon = 'icon-naceita.png'
+        (comando) ? icon = 'icon-aceita.png' : icon = 'icon-naceita.png'
         output.append(
             `<div class="output-item">
                 <p class="entrada-out">${entrada}</p>
@@ -78,69 +90,115 @@ class HandleUI {
                 <p class="description">${mensagem}</p>
             </div>`
         )
-    }
-
-    static clear_afd_output() {
-    }
-
-    
-
-    static transicao_grafo(estado_atual, estado_anterior) {
-        let nodes = $('#graph-afd .node'),
-            edges = $('#graph-afd .edge')
-        let next_node, old_node, curr_edge
-
-        for (let i = 0; i < nodes.length; i++) {
-            let node_id = $(nodes[i]).attr('id')
-            if(node_id === estado_atual){
-                next_node = $(nodes[i])
-            }else if(node_id === estado_anterior) {
-                old_node = $(nodes[i])
-            }
-        }
-
-        for (let j = 0; j < edges.length; j++) {
-            let edge_id = $(edges[j]).attr('id'),
-            look_edge = estado_anterior+ '-' + estado_atual
-            if(edge_id === look_edge) {
-                curr_edge = $(edges[j])                
-                break;
-            }
-        }
-
-        old_node.addClass('active');
-        setTimeout(() => {  curr_edge.addClass('active'); }, 1000);
-        setTimeout(() => {  next_node.addClass('active'); }, 2000);
+        output_count += 1
     }
 }
 
+/* Funções assincronas p/ animar o automato */
+async function wait_for_me(time) {
+    await new Promise(done => setTimeout(() => done(), time));
+}
+
+async function paint_afd(historico) {
+    let curr_node, curr_edge, next_node
+
+    $('#qi').addClass('active');
+    $('#edgeqi').addClass('active');
+
+    for (let i = 0; i < historico.length - 1; i++) {
+        curr_node = '#' + historico[i]
+        curr_edge = '#' + historico[i] + '-' + historico[i + 1]
+        next_node = '#' + historico[i + 1]
+
+        $(curr_node).addClass('active');
+        // await new Promise(done => setTimeout(() => done(), 1000));
+        await wait_for_me(1000)
+        $(curr_edge).addClass('active');
+        // await new Promise(done => setTimeout(() => done(), 1000));
+        await wait_for_me(1000)
+        $(next_node).addClass('active');
+    }
+}
+
+async function clear_afd_output() {
+    await wait_for_me(500)
+
+    let nodes = $('.node'), edges = $('.edge'),
+        active_n, active_e
+
+    for (let i = 0; i < nodes.length; i++) {
+        active_n = $(nodes[i]).hasClass('active')
+        if (active_n)
+            $(nodes[i]).removeClass('active')
+    }
+
+    for (let i = 0; i < edges.length; i++) {
+        active_e = $(edges[i]).hasClass('active')
+        if (active_e)
+            $(edges[i]).removeClass('active')
+    }
+}
+
+function afd_action(afd, entrada) {
+
+    let fita = entrada.match(/[^,\s?]+/g),
+        estado_atual, estado_anterior, fim_fita = fita.length - 1,
+        i, mensagem = '', comando = false
+
+    estado_atual = afd.inicial
+    HandleUI.output_inicial(estado_atual)
+    let historico = [estado_atual]
+    for (i = 0; i < fita.length; i++) {
+        if (afd.exists_in_alf(fita[i])) {
+            estado_anterior = estado_atual
+            estado_atual = afd.le_transicao(estado_atual, fita[i])
+
+            historico.push(estado_atual)
+            if (estado_atual === 'Indefinido') {
+                //jquery indefine algo na ui
+                HandleUI.out_transicao('i', estado_anterior, fita[i], estado_atual)
+                mensagem = 'Simbolo Indf. no estado ' + estado_anterior
+                comando = false
+                break
+            } else if (!afd.estado_final(estado_atual) && i === fim_fita) {
+                //jquery nao-final warning na ui
+                HandleUI.out_transicao('nf', estado_anterior, fita[i], estado_atual)
+                mensagem = 'Fim da Fita em Estado Não Final'
+                comando = false
+                break
+            }
+
+            if (afd.estado_final(estado_atual) && i === fim_fita) {
+                //jquery palavra aceita na ui
+                HandleUI.out_transicao('f', estado_anterior, fita[i], estado_atual)
+                mensagem = 'Palavra Aceita!'
+                comando = true
+                break
+            }
+            HandleUI.out_transicao('next', estado_anterior, fita[i], estado_atual)
+        } else {
+            mensagem = 'Indf. - Simbolo ∉ Alfabeto.'
+            comando = false
+            break
+        }
+    }
+
+    return {historico: historico, comando: comando, mensagem: mensagem}
+}
+
+
+/* Leitura do conteudo do arquivo
+* qd ele é carregado na interface */
 let file_content = ""
-document.getElementById('inputfile')
-    .addEventListener('change', function () {
-        let fr = new FileReader();
-        fr.onload = function () {
-            // document.getElementById('output')
-            //     .textContent = fr.result;
-            file_content = fr.result
-        }
-        fr.readAsText(this.files[0]);
-    })
-
-
-function load_file_afd() {
-    if (!file_content) {
-        console.log('please insert file')
-    } else {
-        console.log(file_content)
+$('#inputfile').change(function () {
+    let fr = new FileReader();
+    fr.onload = function () {
+        file_content = fr.result.split(/\r\n/);
     }
-}
+    fr.readAsText(this.files[0]);
+})
 
-function single_entry() {
-    let user_input = $('#inputEntrada'), element = '#single-output'
-    if (!user_input.val()) {
-        console.log('insert valid input')
-        return 0
-    }
+function inicia_afd(comando) {
     const afd = new Automato(
         ['>q0', 'q1', 'q2', 'q1q2', 'qf*'],
         {
@@ -153,51 +211,44 @@ function single_entry() {
         ['7', '9a', '0b']
     );
 
-    let fita = user_input.val().match(/[^,\s?]+/g),
-        estado_atual = "", estado_anterior,
-        fim_fita = fita.length - 1, i = 0
-
-    estado_atual = afd.inicial
-    HandleUI.output_inicial(estado_atual)
-    for (i = 0; i < fita.length; i++) {
-        if (afd.exists_in_alf(fita[i])) {
-            estado_anterior = estado_atual
-            estado_atual = afd.le_transicao(estado_atual, fita[i])
-
-            //faz transição na ui
-            HandleUI.transicao_grafo(estado_atual, estado_anterior)
-            if (estado_atual === 'Indefinido') {
-                //jquery indefine algo na ui
-                HandleUI.out_transicao('i', estado_anterior, fita[i], estado_atual)
-                HandleUI.log_final(false, fita.join(''),
-                    'Simbolo Indf. no estado ' + estado_anterior, element)
-                break
-            } else if (!afd.estado_final(estado_atual) && i === fim_fita) {
-                //jquery nao-final warning na ui
-                HandleUI.out_transicao('nf', estado_anterior, fita[i], estado_atual)
-                HandleUI.log_final(false, fita.join(''),
-                    'Fim da Fita em Estado Não Final', element)
-                break
+    let data
+    switch (comando) {
+        case 'arquivo':
+            if (!file_content) {
+                console.log('insert valid file')
+                return 0
             }
 
-            if (afd.estado_final(estado_atual) && i === fim_fita) {
-                //jquery palavra aceita na ui
-                HandleUI.out_transicao('f', estado_anterior, fita[i], estado_atual )
-                HandleUI.log_final(true, fita.join(''),'Palavra Aceita!', element)
-                break
+            const forLoop = async _ => {
+                for (let i = 0; i < file_content.length; i++) {
+                    data = afd_action(afd, file_content[i])
+                    await paint_afd(data.historico)
+                    await wait_for_me(1000)
+                    HandleUI.log_final(data.comando, file_content[i],
+                        data.mensagem, '#file-output')
+                    await clear_afd_output()
+                }
             }
-
-            HandleUI.out_transicao(
-                'next', estado_anterior,
-                fita[i], estado_atual
-            )
-
-        } else {
-            HandleUI.log_final(false, fita.join(''),
-                'Indf. - Simbolo ∉ Alfabeto.', element)
+            forLoop()
             break
-        }
+        case 'input':
+            let user_input = $('#inputEntrada'),
+                fita = user_input.val()
+            if (!fita) {
+                console.log('insert valid input')
+                return 0
+            }
+            data = afd_action(afd, fita)
+            user_input.val('')
+            const paint_clear = async _ => {
+                await paint_afd(data.historico)
+                await wait_for_me(1000)
+                HandleUI.log_final(data.comando, fita,
+                    data.mensagem, '#single-output')
+                await clear_afd_output()
+            }
+            paint_clear()
+            break
     }
-    user_input.val('')
 }
 
--- custom.js
+++ custom.js
@@ -1,7 +1,9 @@
-// All jquery here
-$(function() {
+// import inicia_afd from './automato'
+
+/* Funções de customização da página */
+$(function () {
     // Handler for .ready() called.
-    $('.nav-link').on("click", function() {
+    $('.nav-link').on("click", function () {
         let nav_itens = $('.nav-link')
 
         for (let i = 0; i < nav_itens.length; i++) {
@@ -12,112 +14,29 @@ $(function() {
     });
 });
 
+function resize_afd_plot() {
+    let window_size = $(window).width(),
+        afd = $('#afd_plot'),
+        size = ""
 
-function zoom_limiting(network, afterZoomLimit) {
-    network.on("zoom", function () { //while zooming
-        if (network.getScale() <= 0.50) {//the limit you want to stop at
-            network.moveTo(afterZoomLimit); //set this limit so it stops zooming out here
-        }
-    });
-}
-
-
-// Graph Example of AFD and AFND
-let nodes, edges, container,
-    data, options, network,
-    afterZoomLimit, minZoom
-
-
-// Define zoom limits
-afterZoomLimit = { //here we are setting the zoom limit to move to
-    scale: 0.70,
-}
-
-// Define min zoom
-minZoom = {
-    scale: 1.5
-}
-
-// Define options for the graphs
-options = {
-    autoResize: true,
-    height: '100%',
-    width: '100%',
-    edges: {
-        arrowStrikethrough: true,
-        arrows: {
-            to: {
-                enabled: true,
-                scaleFactor: 1,
-                type: "arrow"
-            },
-
-        },
-        smooth: {
-            enabled: true,
-            forceDirection: true,
-            type: 'curvedCW',
-            roundness: 0.4
-        }
-    },
-
-    interaction: {
-        dragNodes: false,
-        // zoomView: false,
-        // dragView: false
-    },
-    physics: false,
-    layout: {
-        hierarchical: {
-            direction: 'UD'
-        }
+    if (window_size < 400) {
+        size = '240pt'
+    } else if (window_size < 470) {
+        size = '300pt'
+    } else if (window_size < 530) {
+        size = '320pt'
+    } else if (window_size < 766) {
+        size = '368pt'
+    } else if (window_size < 982) {
+        size = '468pt'
     }
-};
+    afd.attr('width', size)
+}
 
+$(window).resize(function () {
+    resize_afd_plot();
+});
 
-
-// -> AFD: Create array of nodes and edges
-nodes = new vis.DataSet([
-    {id: 0, label: "S0", color: { background: "pink" }},
-    {id: 1, label: "S1"},
-    {id: 2, label: "S2", shapeProperties: { borderDashes: [5, 2] }, borderWidth: 2},
-]);
-
-
-edges = new vis.DataSet([
-    {from: 0, to: 0, label: '0'},
-    {from: 0, to: 1, label: '1'},
-    {from: 1, to: 0, label: '0'},
-    {from: 1, to: 2, label: '0'},
-    {from: 2, to: 1, label: '0'},
-    {from: 2, to: 2, label: '1'}
-]);
-
-data = {
-    nodes: nodes,
-    edges: edges
-};
-
-// Create a network
-container = document.getElementById("mynetwork");
-network = new vis.Network(container, data, options);
-network.moveTo(minZoom);
-zoom_limiting(network, afterZoomLimit)
-
-// -> AFND: Create array of nodes and edges
-edges = new vis.DataSet([
-    {from: 0, to: 1, label: '1', smooth: { type: 'curvedCCW' }},
-    {from: 0, to: 2, label: '1'},
-    {from: 1, to: 2, label: '0', smooth: { type: 'curvedCCW' }},
-    {from: 2, to: 2, label: '2'}
-]);
-
-data = {
-    nodes: nodes,
-    edges: edges
-};
-
-container = document.getElementById("mynetwork2");
-network = new vis.Network(container, data, options);
-network.moveTo(minZoom);
-zoom_limiting(network, afterZoomLimit)
+$(document).ready(function () {
+    resize_afd_plot();
+});
